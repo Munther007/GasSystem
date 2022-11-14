358cbcefe20cfeac471b290876db73b56df6e19e
AthraaOil <Athraa.oil369@gmail.com>
2022-11-14 12:06:57 +0300

    second done

--- resources/views/cars/allCars.blade.php
+++ resources/views/cars/allCars.blade.php
@@ -6,6 +6,7 @@
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
 
     <link rel="stylesheet" href="{{asset('css/style2.css')}}">
@@ -15,25 +16,30 @@
 
 <body dir="rtl">
 
-<main>
-    <div class="top">
-        <!-- <i class="uil uil-bars sidebar-toggle"></i> -->
-        <div class="btn_top">
-            <a href="/cars/export">
-            <button class="btnRegister33">تصدير كملف</button>
-            </a>
-            <a href="/print-cars">
-            <button class="btnRegister33 btn_rigester">طباعة الملف</button>
-            </a>
-        </div>
-        <div class="search-box">
-            <input type="text" placeholder="أبحث هنا...">
-            <i class="uil uil-search"></i>
-        </div>
-
-        <!--<img src="images/profile.jpg" alt="">-->
+<div class="container">
+    <div class="row">
     </div>
-    <br>
+</div>
+<div class="container mt-2">
+    <div class="row mt-2 justify">
+                <div class="col-6">
+                    <button class="btnRegister33">
+                        <a href="/cars/export"> تصدير كملف</a>
+                    </button>
+                    <button class="btnRegister33 btn_rigester">
+                        <a href="/print-cars">طباعة الملف</a>
+                    </button>
+                </div>
+
+            <div class="col-3">
+                <form class="search-box d-flex " role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"> <space></space>
+                    <button class="btn btn-outline-success" type="submit">بحث</button>
+                </form>
+            </div>
+    </div>
+</div>
+
 
     <h1 class="title_table">  معلومات البطاقات </h1>
     <table>
@@ -75,6 +81,9 @@
             </th>
             <th>
                 الملاحظات
+            </th>
+            <th>
+                العمليات
             </th>
         </tr>
         </thead>
@@ -142,7 +151,8 @@
 <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 
 <script src="{{asset('js/index.js')}}"></script>
-
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
 </body>
 </html>
 
